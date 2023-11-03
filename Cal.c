2b9b482de957d73c0253d6225365f44f53a2c560
ajinkya4422 <147471763+ajinkya4422@users.noreply.github.com>
2023-11-03 05:20:27 +0000

    Feature of decimal to hexadecimal added

--- Cal.c
+++ Cal.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     char operator;
-    double num1, num2;
+    double num1, num2, result;
 
-    printf("Enter an operator (+, -, *, /): ");
+    printf("Enter an operator (+, -, *, /, ^ for exponentiation, h for hexadecimal conversion): ");
     scanf(" %c", &operator);
 
-    printf("Enter two numbers: ");
-    scanf("%lf %lf", &num1, &num2);
+    if (operator != 'h') {
+        printf("Enter two numbers: ");
+        scanf("%lf %lf", &num1, &num2);
+    }
 
     switch (operator) {
         case '+':
-            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            result = num1 + num2;
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
             break;
         case '-':
-            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            result = num1 - num2;
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
             break;
         case '*':
-            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            result = num1 * num2;
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
             break;
         case '/':
             if (num2 != 0) {
-                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+                result = num1 / num2;
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
             } else {
                 printf("Error: Division by zero is not allowed.\n");
             }
@@ -30,6 +37,12 @@ int main() {
         case '^':
             result = pow(num1, num2);
             printf("%.2lf ^ %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case 'h':
+            printf("Enter a decimal number: ");
+            scanf("%lf", &num1);
+            int hexValue = (int)num1;
+            printf("%.2lf in hexadecimal is: %X\n", num1, hexValue);
             break;
         default:
             printf("Error: Invalid operator.\n");
